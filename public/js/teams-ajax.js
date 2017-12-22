f089921b933ea7e29f01fffc3f5d83c20423319d
rallysport206 <phan.tony125@gmail.com>
2017-12-22 11:16:55 -0800

    updated with class

--- public/js/teams-ajax.js
+++ public/js/teams-ajax.js
@@ -1,11 +1,25 @@
-
-$('.delete-link').click(function(e) {
-  e.preventDefault();
-  $.ajax({
-    url: $(this).attr('href'),
-    method: 'DELETE'
-  }).success(function(data){
-    window.location.href = '/teams';
+$(document).ready(function () {
+  $('.delete-link').click(function(e) {
+    e.preventDefault();
+    $.ajax({
+      method: 'DELETE',
+      url: $(this).attr('href')
+    }).success(function(data){
+      window.location.href = '/teams';
+    });
+  });
+  $('.edit-form').submit(function(e) {
+    e.preventDefault();
+    $.ajax({
+      method: 'PUT',
+      url: $(this).attr('action'),
+      data: {
+        name: $('#new-name').val(),
+        members: $('#members').val()
+      }
+    }).done(function(response) {
+      window.location.href = '/teams';
+    });
   });
 });
 // //editing team names
